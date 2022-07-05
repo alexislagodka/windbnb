cab195ac2c8337b63c4752acec16c3c1e65d8fc4
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2022-07-05 10:37:53 +0200

    bg home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import { BsFillTriangleFill } from "react-icons/bs";
 export default function Home() {
   return (
     <main>
-      <section className="bg-gradient-to-b from-salmon to-yellow-300 h-full flex flex-col items-center justify-around">
+      <section className="h-full flex flex-col items-center justify-around bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')]">
         <h1 className="text-white flex">
           Welcome to &nbsp;
           <BsFillTriangleFill />
